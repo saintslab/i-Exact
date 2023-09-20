b4d02efff1c1e6a896f9b8e30152f484cd845216
Sebastian <sebeliassen@gmail.com>
2023-09-20 22:00:29 +0200

    exact folder added

---
+++ exact/build/lib.linux-x86_64-cpython-38/exact/conf.py
@@ -0,0 +1,28 @@
+class QuantizationConfig:
+    def __init__(self):
+        self.compress_activation = True
+        self.activation_compression_bits = [2]
+        self.initial_bits = 8
+        self.stochastic = True
+        self.use_gradient = False
+        self.simulate = False
+        self.enable_quantized_bn = True
+        self.kept_frac = 1.0
+        self.single_precision = True
+        self.dropout2 = False
+        self.col_size = 0
+        self.last_epoch = False
+        self.lo = 1
+        self.use_optimal_lo = False
+        # Memory management flag
+        self.empty_cache_threshold = None
+        self.swap = False
+        self.amp = False
+        # Debug related flag
+        self.debug_memory_op_forward = False
+        self.debug_memory_op_backward = False
+        self.debug_remove_bn = False
+        self.debug_remove_relu = False
+        self.debug_remove_dropout = False
+
+config = QuantizationConfig()
